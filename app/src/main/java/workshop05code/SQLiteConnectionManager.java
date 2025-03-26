cbf0d1d02586757ae49addfd2eab975a1b736891
Keisha <quichejunk@gmail.com>
2025-03-27 00:03:37 +1100

    fix(IsValidWord): Add SQL parameterisation

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -145,10 +145,12 @@ public class SQLiteConnectionManager {
      * @return true if guess exists in the database, false otherwise
      */
     public boolean isValidWord(String guess) {
-        String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
+        // Change concatenated string to a prepared statement with a ? placeholder
+        String sql = "SELECT count(id) as total FROM validWords WHERE word like ?"; 
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, guess); //Add guess to the prepared statement (replace '?'')
 
             ResultSet resultRows = stmt.executeQuery();
             if (resultRows.next()) {
